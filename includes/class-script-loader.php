6e2c0cd2a314c2c7a988dab417cc3b0c1e48e032
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-05-12 23:32:15 +0800

    add swiper for dev

--- includes/class-script-loader.php
+++ includes/class-script-loader.php
@@ -2,20 +2,24 @@
 
 namespace Feebas;
 
-class Script_Loader {
+class Script_Loader
+{
 
-    public static function register() {
-        add_action( 'elementor/frontend/after_register_scripts', [ __CLASS__, 'register_scripts' ] );
-        add_action( 'elementor/editor/after_enqueue_scripts', [ __CLASS__, 'register_scripts' ] );
-        add_action( 'elementor/editor/after_enqueue_scripts', [ __CLASS__, 'enqueue_editor_scripts' ] );
-        add_filter( 'script_loader_tag', [ __CLASS__, 'modify_script_tag' ], 10, 3 );
+    public static function register()
+    {
+        add_action('elementor/frontend/after_register_scripts', [__CLASS__, 'register_scripts']);
+        add_action('elementor/editor/after_enqueue_scripts', [__CLASS__, 'register_scripts']);
+        add_action('elementor/editor/after_enqueue_scripts', [__CLASS__, 'enqueue_editor_scripts']);
+        add_filter('script_loader_tag', [__CLASS__, 'modify_script_tag'], 10, 3);
+        add_action('wp_head', [__CLASS__, 'enqueue_swiper']);
     }
 
-    public static function register_scripts() {
+    public static function register_scripts()
+    {
         wp_register_script(
             'feebas-asset-viewer',
             FEW_PLUGIN_URL . 'widgets/js/asset-viewer.js',
-            [ 'elementor-frontend' ],
+            ['elementor-frontend'],
             null,
             true
         );
@@ -23,7 +27,7 @@ class Script_Loader {
         wp_register_script(
             'feebas-horizontal-cards',
             FEW_PLUGIN_URL . 'widgets/js/horizontal-cards.js',
-            [ 'jquery' ],
+            ['jquery'],
             null,
             true
         );
@@ -34,21 +38,30 @@ class Script_Loader {
             [],
             null
         );
+
     }
 
-    public static function enqueue_editor_scripts() {
-        wp_enqueue_script( 'feebas-horizontal-cards' );
-        wp_localize_script( 'feebas-horizontal-cards', 'FeebasHorizontalCardsSettings', [
-            'ajax_url' => admin_url( 'admin-ajax.php' ),
-            'nonce' => wp_create_nonce( 'feebas_widget_nonce' )
+    public static function enqueue_editor_scripts()
+    {
+        wp_enqueue_script_module('feebas-horizontal-cards');
+        wp_localize_script('feebas-horizontal-cards', 'FeebasHorizontalCardsSettings', [
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('feebas_widget_nonce')
         ]);
     }
 
-    public static function modify_script_tag( $tag, $handle, $src ) {
-        if ( $handle === 'feebas-asset-viewer' ) {
+    public static function modify_script_tag($tag, $handle, $src)
+    {
+        if ($handle === 'feebas-asset-viewer') {
             return '<script type="importmap">{"imports":{"three":"https://unpkg.com/three@0.152.2/build/three.module.js"}}</script>' .
-                sprintf( '<script type="module" src="%s"></script>', esc_url( $src ) );
+                sprintf('<script type="module" src="%s"></script>', esc_url($src));
         }
         return $tag;
+    }
+
+    public static function enqueue_swiper()
+    {
+        echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />';
+        echo '<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>';
     }
 }
